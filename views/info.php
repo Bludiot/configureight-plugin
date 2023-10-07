f747e54f2390a4872351c2fcce002653fa24c933
ControlledChaos <greg@ccdzine.com>
2023-10-07 11:32:14 -0700

    Plugin info.

--- views/info.php
+++ views/info.php
@@ -11,3 +11,9 @@
 <?php echo Bootstrap :: formTitle( [ 'title' => $L->g( 'Theme Information' ) ] ); ?>
 
 <?php echo Bootstrap :: formTitle( [ 'title' => $L->g( 'Available Templates' ) ] ); ?>
+
+<?php echo Bootstrap :: formTitle( [ 'title' => $L->g( 'Support' ) ] ); ?>
+
+<p><a href="https://github.com/Bludiot" target="_blank" rel="noopener noreferrer">https://github.com/Bludiot</a></p>
+
+<p><?php $L->p( 'Let me know if you need me to develop a custom version of the Configure 8 theme for you.' ); ?></p>

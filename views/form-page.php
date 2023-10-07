4416b934adf11f94b57691d88b1079aec4162b0a
ControlledChaos <greg@ccdzine.com>
2023-10-06 19:21:26 -0700

    Markup and class names like other settings page.

--- views/form-page.php
+++ views/form-page.php
@@ -1,50 +1,47 @@
 <?php
 /**
  * Settings form template
+ *
+ * @package    Configure 8 Settings
+ * @subpackage Views
+ * @since      1.0.0
  */
 
 // Page description from JSON metadata.
 echo $plugin->description();
 
 ?>
-<div class="tabs" data-toggle="tabslet" data-animation="true">
+<div class="tab-content" data-toggle="tabslet" data-animation="true">
 
-	<ul class="tabs-list">
-		<li class="tab active"><a href="#general">General</a></li>
-		<li class="tab"><a href="#header">Header</a></li>
-		<li class="tab"><a href="#media">Media</a></li>
-		<li class="tab"><a href="#loop">Loop</a></li>
-		<li class="tab"><a href="#sidebar">Sidebar</a></li>
-		<li class="tab"><a href="#footer">Footer</a></li>
-		<li class="tab"><a href="#styles">Styles</a></li>
+	<ul class="nav nav-tabs">
+		<li class="nav-item active"><a class="nav-link" href="#general">General</a></li>
+		<li class="nav-item"><a class="nav-link" href="#header">Header</a></li>
+		<li class="nav-item"><a class="nav-link" href="#media">Media</a></li>
+		<li class="nav-item"><a class="nav-link" href="#loop">Loop</a></li>
+		<li class="nav-item"><a class="nav-link" href="#sidebar">Sidebar</a></li>
+		<li class="nav-item"><a class="nav-link" href="#footer">Footer</a></li>
+		<li class="nav-item"><a class="nav-link" href="#styles">Styles</a></li>
 	</ul>
 
-	<div id="general" class="tab-content">
-
-		<h3>General Options</h3>
-
-		<label class="form-label" for="page_loader"><?php $L->p('Page Loader'); ?></label>
-		<select class="form-select" id="page_loader" name="page_loader">
-			<option value="false" <?php ( $this->getValue( 'page_loader' ) === false ? 'selected' : '' ); ?>><?php $L->p( 'Disabled' ); ?></option>
-			<option value="true" <?php ( $this->getValue( 'page_loader' ) === true ? 'selected' : '' ); ?>><?php $L->p( 'Enabled' ); ?></option>
-		</select>
+	<div id="general" class="tab-pane active">
+		<?php include( $this->phpPath() . '/views/fields-general.php' ); ?>
 	</div>
-	<div id="header" class="tab-content">
+	<div id="header" class="tab-pane">
 		<h3>Header Options</h3>
 	</div>
-	<div id="media" class="tab-content">
+	<div id="media" class="tab-pane">
 		<h3>Media Options</h3>
 	</div>
-	<div id="loop" class="tab-content">
+	<div id="loop" class="tab-pane">
 		<h3>Loop Options</h3>
 	</div>
-	<div id="sidebar" class="tab-content">
+	<div id="sidebar" class="tab-pane">
 		<h3>Sidebar Options</h3>
 	</div>
-	<div id="footer" class="tab-content">
+	<div id="footer" class="tab-pane">
 		<h3>Footer Options</h3>
 	</div>
-	<div id="styles" class="tab-content">
+	<div id="styles" class="tab-pane">
 		<h3>Theme Styles</h3>
 	</div>
 </div>

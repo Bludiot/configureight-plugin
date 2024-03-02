9c8bfc2719d072ddc366cf9f83ddbb3e5eb28c4a
ControlledChaos <greg@ccdzine.com>
2024-03-02 13:18:42 -0800

    Add Pages List plugin link.

--- views/dash-widget-customize.php
+++ views/dash-widget-customize.php
@@ -29,6 +29,9 @@ $categories_settings = DOMAIN_ADMIN . 'configure-plugin/Categories_Lists';
 // Tags settings page URL.
 $tags_settings = DOMAIN_ADMIN . 'configure-plugin/Tags_Lists';
 
+// Pages settings page URL.
+$pages_settings = DOMAIN_ADMIN . 'configure-plugin/Pages_Lists';
+
 // Search settings page URL.
 $search_settings = DOMAIN_ADMIN . 'configure-plugin/Search_Forms';
 
@@ -55,15 +58,18 @@ $search_settings = DOMAIN_ADMIN . 'configure-plugin/Search_Forms';
 		<li><a href="<?php echo $guide_page; ?>"><?php $L->p( 'Theme Guide' ); ?></a></li>
 		<li><a href="<?php echo $colors_page; ?>"><?php $L->p( 'Colors Reference' ); ?></a></li>
 		<li><a href="<?php echo $fonts_page; ?>"><?php $L->p( 'Fonts Reference' ); ?></a></li>
-		<li><a href="<?php echo $database_page; ?>"><?php $L->p( 'Options Database' ); ?></a></li>
+		<?php if ( getPlugin( 'Search_Forms' ) ) : ?>
+		<li><a href="<?php echo $search_settings; ?>"><?php $L->p( 'Search Options' ); ?></a></li>
+		<?php endif; ?>
 		<?php if ( getPlugin( 'Categories_Lists' ) ) : ?>
 		<li><a href="<?php echo $categories_settings; ?>"><?php $L->p( 'Categories Options' ); ?></a></li>
 		<?php endif; ?>
 		<?php if ( getPlugin( 'Tags_Lists' ) ) : ?>
 		<li><a href="<?php echo $tags_settings; ?>"><?php $L->p( 'Tags Options' ); ?></a></li>
 		<?php endif; ?>
-		<?php if ( getPlugin( 'Search_Forms' ) ) : ?>
-		<li><a href="<?php echo $search_settings; ?>"><?php $L->p( 'Search Options' ); ?></a></li>
+		<?php if ( getPlugin( 'Pages_Lists' ) ) : ?>
+		<li><a href="<?php echo $pages_settings; ?>"><?php $L->p( 'Pages Options' ); ?></a></li>
 		<?php endif; ?>
+		<li><a href="<?php echo $database_page; ?>"><?php $L->p( 'Options Database' ); ?></a></li>
 	</ul>
 </div>

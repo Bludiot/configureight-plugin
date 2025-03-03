05c2c00d7417b1b0eb03b34bfb38d71cb60bc1d6
ControlledChaos <greg@ccdzine.com>
2025-03-03 08:40:29 -0800

    Edits to guide intro tab.

--- views/info-intro.php
+++ views/info-intro.php
@@ -10,24 +10,12 @@
  * @todo Monitor accuracy of translation message.
  */
 
-// Theme name.
-$theme = 'Configure 8';
-
-// Color schemes page URL.
-$colors_page = DOMAIN_ADMIN . 'plugin/' . $this->className() . '?page=colors';
-
-// Font schemes page URL.
-$fonts_page = DOMAIN_ADMIN . 'plugin/' . $this->className() . '?page=fonts';
-
-// Database page URL.
+// Guide page URLs.
+$colors_page   = DOMAIN_ADMIN . 'plugin/' . $this->className() . '?page=colors';
+$fonts_page    = DOMAIN_ADMIN . 'plugin/' . $this->className() . '?page=fonts';
 $database_page = DOMAIN_ADMIN . 'plugin/' . $this->className() . '?page=database';
 
-//Search plugin URLs.
-$s_settings = DOMAIN_ADMIN . 'configure-plugin/Search_Forms';
-$s_guide    = DOMAIN_ADMIN . 'plugin/Search_Forms';
-
 ?>
-
 <h2 class="form-heading"><?php $L->p( 'Reference Pages' ); ?></h2>
 
 <p><?php $L->p( 'This options guide includes several separate reference pages:' ); ?></p>
@@ -39,27 +27,11 @@ $s_guide    = DOMAIN_ADMIN . 'plugin/Search_Forms';
 
 <h2 class="form-heading"><?php $L->p( 'Compatibility' ); ?></h2>
 
-<p><?php $L->p( "The {$theme} theme was designed with great extensibility by plugins. All of the core plugin hooks are included and placed in appropriate positions in the various template parts. That said, we cannot maintain compatibility for all plugins nor take responsibility for poorly developed plugins. If there is a plugin hook you would like to have us include, please see the support tab." ); ?></p>
+<p><?php $L->p( 'The Configure 8 theme was designed with great extensibility by plugins. All of the core plugin hooks are included and placed in appropriate positions in the various template parts. That said, we cannot maintain compatibility for all plugins nor take responsibility for poorly developed plugins. If there is a plugin hook you would like to have us include, please see the support tab.' ); ?></p>
 
 <p><?php $L->p( 'Custom plugin hooks have been created and are included included in various template locations, where features and supplemental content may be displayed.' ); ?></p>
 
-<h2 class="form-heading"><?php $L->p( 'Search Functionality' ); ?></h2>
-
-<p><?php $L->p( "The core search plugin, simply named 'Search', is supported and will display a search form in the site sidebar. However, to take full advantage of the search form options, we recommend using the 'Search Forms' plugin, developed by us for use in the {$theme} theme." ); ?></p>
-
-<?php if ( ! getPlugin( 'Search_Forms' ) ) : ?>
-
-<p><?php $L->p( 'There can only be one functional instance of the core search plugin per page without causing a JavaScript conflict. The Search Forms plugin creates unique JavaScript for each instance of a search form, so more than one form may be used on one page. Also there are more options available for each form, including options for the sidebar search form which require no coding.' ); ?></p>
-
-<p><?php $L->p( "If you downloaded the full {$theme} suite then you should have received a copy of Search Forms. Or find it at <a href='https://github.com/Bludiot/searchforms' target='_blank' rel='noopener noreferrer'>https://github.com/Bludiot/searchforms</a>" ); ?></p>
-
-<?php else : ?>
-
-<p><?php $L->p( '<strong>Good news!</strong> You have the Search Forms plugin installed.' ); ?></p>
-
-<p><?php $L->p( "Go to the <a href='{$s_settings}'>search settings</a> page. Developers, go to the <a href='{$s_guide}'>search guide</a> page." ); ?></p>
-
-<?php endif; ?>
+<p><?php $L->p( 'See the Plugins tab for plugins specifically developed to enhance Configure 8.' ); ?></p>
 
 <h2 class="form-heading"><?php $L->p( 'Languages' ); ?></h2>
 
@@ -71,10 +43,4 @@ $s_guide    = DOMAIN_ADMIN . 'plugin/Search_Forms';
 
 <p><?php $L->p( 'Please register errors, conflicts, or other problems with the Configure 8 suite of products under the Issues tab in the relevant GitHub repository.' ); ?></p>
 
-<p><strong class="semi-bold"><?php $L->p( 'Theme:' ); ?></strong> <a href="https://github.com/Bludiot/configureight" target="_blank" rel="noopener noreferrer">https://github.com/Bludiot/configureight</a></p>
-
-<p><strong class="semi-bold"><?php $L->p( 'Plugin:' ); ?></strong> <a href="https://github.com/Bludiot/configureight-plugin" target="_blank" rel="noopener noreferrer">https://github.com/Bludiot/configureight-plugin</a></p>
-
-<p><strong class="semi-bold"><?php $L->p( 'Admin:' ); ?></strong> <a href="https://github.com/Bludiot/configureight-admin" target="_blank" rel="noopener noreferrer">https://github.com/Bludiot/configureight-admin</a></p>
-
-<p><strong class="semi-bold"><?php $L->p( 'Search:' ); ?></strong> <a href="https://github.com/Bludiot/searchforms" target="_blank" rel="noopener noreferrer">https://github.com/Bludiot/searchforms</a></p>
+<p><?php $L->p( 'Find Configure 8 product development at' ); ?> <a href="https://github.com/Bludiot" target="_blank" rel="noopener noreferrer">https://github.com/Bludiot</a></p>

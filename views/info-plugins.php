f6c8cd7b589a0cf456d668e3963a7dafb2a65a13
Controlled Chaos Design <greg@ccdzine.com>
2025-11-20 13:04:17 -0800

    Change tabs order.

--- views/info-plugins.php
+++ views/info-plugins.php
@@ -28,79 +28,18 @@ use function CFE_Plugin\{
 <div class="tab-content" data-toggle="tabslet" data-deeplinking="false" data-animation="true">
 
 	<ul class="nav nav-tabs" id="nav-tabs" role="tablist">
-		<li class="nav-item">
-			<a class="nav-link" role="tab" aria-controls="active" aria-selected="false" href="#active"><?php lang()->p( 'Active' ); ?></a>
+		<li class="nav-item active">
+			<a class="nav-link" role="tab" aria-controls="inactive" aria-selected="false" href="#inactive"><?php lang()->p( 'Inactive' ); ?></a>
 		</li>
 		<li class="nav-item">
-			<a class="nav-link" role="tab" aria-controls="inactive" aria-selected="false" href="#inactive"><?php lang()->p( 'Inactive' ); ?></a>
+			<a class="nav-link" role="tab" aria-controls="active" aria-selected="false" href="#active"><?php lang()->p( 'Active' ); ?></a>
 		</li>
 		<li class="nav-item">
 			<a class="nav-link" role="tab" aria-controls="downloads" aria-selected="false" href="#downloads"><?php lang()->p( 'Downloads' ); ?></a>
 		</li>
 	</ul>
-	<div id="active" class="tab-pane" role="tabpanel" aria-labelledby="active">
 
-		<h2 class="form-heading"><?php lang()->p( 'Active Companion Plugins' ); ?></h2>
-
-		<p><?php lang()->p( 'The following Configure 8 companion plugins are installed and activated.' ); ?></p>
-
-		<?php
-		/**
-		 * List active companion plugins
-		 *
-		 * Includes plugin name, custom description
-		 * from plugin metadata file, and options link.
-		 */
-		$count = 0;
-		foreach ( suite_plugins_active() as $plugin ) {
-
-			$count++;
-			$plugin = getPlugin( $plugin );
-
-			if ( ! $plugin ) {
-				continue;
-			}
-
-			printf(
-				'<h3 class="form-heading">%s</h3>',
-				$plugin->name()
-			);
-			if ( isset( $plugin->metadata['theme_desc'] ) ) {
-				printf(
-				'<p>%s</p>',
-				$plugin->metadata['theme_desc']
-				);
-			}
-
-			$suite_plugin = suite_plugin_item( $plugin->className() );
-			if ( $suite_plugin['guide'] ) {
-
-				$guide_url = plugin_guide_url( $plugin->className() );
-				if ( is_string( $suite_plugin['guide'] ) ) {
-					$guide_url = plugin_guide_url( $plugin->className() ) . $suite_plugin['guide'];
-				}
-				printf(
-					'<p><a href="%s" class="button btn btn-primary btn-sm">%s</a> <a href="%s" class="button btn btn-secondary btn-sm">%s</a></p>',
-					plugin_options_url( $plugin->className() ),
-					lang()->get( 'Options' ),
-					$guide_url,
-					lang()->get( 'Guide' )
-				);
-			} else {
-				printf(
-					'<p><a href="%s" class="button btn btn-primary btn-sm">%s</a></p>',
-					plugin_options_url( $plugin->className() ),
-					lang()->get( 'Options' )
-				);
-			}
-
-			if ( $count < count( suite_plugins_active() ) ) {
-				echo '<hr />';
-			}
-		}
-		?>
-	</div>
-	<div id="inactive" class="tab-pane" role="tabpanel" aria-labelledby="inactive">
+	<div id="inactive" class="tab-pane active" role="tabpanel" aria-labelledby="inactive">
 
 		<h2 class="form-heading"><?php lang()->p( 'Inactive Companion Plugins' ); ?></h2>
 
@@ -169,6 +108,70 @@ use function CFE_Plugin\{
 		}
 		?>
 	</div>
+
+	<div id="active" class="tab-pane" role="tabpanel" aria-labelledby="active">
+
+		<h2 class="form-heading"><?php lang()->p( 'Active Companion Plugins' ); ?></h2>
+
+		<p><?php lang()->p( 'The following Configure 8 companion plugins are installed and activated.' ); ?></p>
+
+		<?php
+		/**
+		 * List active companion plugins
+		 *
+		 * Includes plugin name, custom description
+		 * from plugin metadata file, and options link.
+		 */
+		$count = 0;
+		foreach ( suite_plugins_active() as $plugin ) {
+
+			$count++;
+			$plugin = getPlugin( $plugin );
+
+			if ( ! $plugin ) {
+				continue;
+			}
+
+			printf(
+				'<h3 class="form-heading">%s</h3>',
+				$plugin->name()
+			);
+			if ( isset( $plugin->metadata['theme_desc'] ) ) {
+				printf(
+				'<p>%s</p>',
+				$plugin->metadata['theme_desc']
+				);
+			}
+
+			$suite_plugin = suite_plugin_item( $plugin->className() );
+			if ( $suite_plugin['guide'] ) {
+
+				$guide_url = plugin_guide_url( $plugin->className() );
+				if ( is_string( $suite_plugin['guide'] ) ) {
+					$guide_url = plugin_guide_url( $plugin->className() ) . $suite_plugin['guide'];
+				}
+				printf(
+					'<p><a href="%s" class="button btn btn-primary btn-sm">%s</a> <a href="%s" class="button btn btn-secondary btn-sm">%s</a></p>',
+					plugin_options_url( $plugin->className() ),
+					lang()->get( 'Options' ),
+					$guide_url,
+					lang()->get( 'Guide' )
+				);
+			} else {
+				printf(
+					'<p><a href="%s" class="button btn btn-primary btn-sm">%s</a></p>',
+					plugin_options_url( $plugin->className() ),
+					lang()->get( 'Options' )
+				);
+			}
+
+			if ( $count < count( suite_plugins_active() ) ) {
+				echo '<hr />';
+			}
+		}
+		?>
+	</div>
+
 	<div id="downloads" class="tab-pane" role="tabpanel" aria-labelledby="downloads">
 
 		<style>
